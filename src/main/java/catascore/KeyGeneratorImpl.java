1e05eb3a325ca500b509951b6a358f7a572d9b46
Remo Caramelle <rcaramel@everis.com>
2018-06-07 16:24:51 +0200

    Pull request changes

--- src/main/java/catascore/KeyGeneratorImpl.java
+++ src/main/java/catascore/KeyGeneratorImpl.java
@@ -3,13 +3,16 @@ package catascore;
 
 public class KeyGeneratorImpl  implements KeyGenerator{
 
-    private int first = 0;
+    private boolean first = true;
     private StringBuffer key = new StringBuffer("AAAAAA");
     private int currentPos = key.length() - 1;
 
+    /**
+     * Generate unique key
+     */
     public String generateKey() throws Exception{
 
-            if (this.first != 0) {
+            if (!this.first) {
                 if (this.key.charAt(this.currentPos) == 'Z') {
                     this.key.setCharAt(this.currentPos, 'A');
                     this.currentPos--;
@@ -20,7 +23,7 @@ public class KeyGeneratorImpl  implements KeyGenerator{
                 char character = key.charAt(this.currentPos);
                 this.key.setCharAt(this.currentPos, ++character);
             }
-            this.first = 1;
+            this.first = false;
             return this.key.toString();
     }
 }
--- src/test/java/catascore/KeyGeneratorImplTest.java
+++ src/test/java/catascore/KeyGeneratorImplTest.java
@@ -1,5 +1,6 @@
 package catascore;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,31 +9,35 @@ import static org.junit.Assert.assertNotEquals;
 public class KeyGeneratorImplTest {
 
     final static int N_CHAR = 6;
+    KeyGenerator keyGenerator;
+
+    @Before
+    public void initialize(){
+        keyGenerator = new KeyGeneratorImpl();
+    }
 
     @Test
     public void generateSixCharacterKey() throws Exception{
-        KeyGenerator keyGenerator = new KeyGeneratorImpl();
         assertEquals(N_CHAR, keyGenerator.generateKey().length());
     }
 
     @Test
     public void generateUppercaseKey() throws Exception{
-        KeyGenerator keyGenerator = new KeyGeneratorImpl();
         String key = null;
         key = keyGenerator.generateKey();
         assertEquals(key.toUpperCase(),key);
     }
 
     @Test
-    public void  generateUniqueKey() throws Exception{
-        KeyGenerator keyGenerator = new KeyGeneratorImpl();
-        assertNotEquals(keyGenerator.generateKey(),keyGenerator.generateKey());
+    public void  generateDistinctKey() throws Exception{
+        String firstKey = keyGenerator.generateKey();
+        String secondKey = keyGenerator.generateKey();
+        assertNotEquals(firstKey,secondKey);
     }
 
     @Test
     public void changeNextLetter() throws Exception{
-        KeyGenerator keyGenerator = new KeyGeneratorImpl();
-            String key;
+        String key;
         do{
                 key = keyGenerator.generateKey();
         }while (key.charAt(key.length() -1) != 'Z');
@@ -44,11 +49,10 @@ public class KeyGeneratorImplTest {
 
     @Test(expected = NotKeyAvailableException.class)
     public void getAfterLastKey() throws Exception {
-            KeyGenerator keyGenerator = new KeyGeneratorImpl();
         String key;
-            do {
-                key = keyGenerator.generateKey();
-            } while (key != "ZZZZZZ");
+        do {
+            key = keyGenerator.generateKey();
+        } while (key != "ZZZZZZ");
         key = keyGenerator.generateKey();
     }
 }
